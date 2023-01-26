c4697f8d4d5c652671c3ff8fadfcf7d184f2f600
Carlholmstrom <54806294+Carlholmstrom@users.noreply.github.com>
2023-01-26 17:42:25 +0100

    Update MilkList.tsx

--- client/src/components/MilkList.tsx
+++ client/src/components/MilkList.tsx
@@ -18,6 +18,8 @@ import {
   CardMedia,
   Typography,
   TextField,
+  Container,
+  Grid,
 } from "@mui/material";
 
 export interface IMilkData {
@@ -87,94 +89,102 @@ const MilkList = () => {
   const numOfMilks = filteredMilkData.length;
 
   return (
-    <section className="milk__container">
-      <ToastContainer
-        autoClose={3000}
-        draggable={false}
-        icon={<CheckCircleIcon />}
-        className="green__toast"
-      />
-      <div className="milk__grid">
-        {selectedMilk === null ? (
-          <form className="milk__search">
-            <label>
-              <TextField
-                sx={{ m: 1, width: 300 }}
-                className="milk__search-input"
-                type="text"
-                placeholder="Search milk..."
-                onChange={handleSearch}
-              />
-            </label>
-
-            <FormControl sx={{ m: 1, width: 300 }}>
-              <InputLabel>Types</InputLabel>
-              <Select
-                value={selectedType}
-                label="Filter by type"
-                onChange={handleTypeSelection}
-              >
-                <MenuItem value="">All</MenuItem>
-                {Array.from(new Set(milkData.map((milk) => milk.type))).map(
-                  (type) => (
-                    <MenuItem value={type}>{type}</MenuItem>
-                  )
-                )}
-              </Select>
-            </FormControl>
-            <div className="milk__counter">
-              <p>{numOfMilks} milks</p>
-            </div>
-          </form>
-        ) : null}
-        {selectedMilk ? (
-          <article className="milk__card">
-            <div className="milk__card-img-container">
-              <img src={MilkImage} alt="milk" className="milk__card-img" />
-            </div>
-          </article>
-        ) : (
-          filteredMilkData.map((milk) => (
-            <Card sx={{ width: 300, m: 2 }}>
-              <CardActionArea onClick={() => setSelectedMilk(milk)}>
-                <CardMedia
-                  component="img"
-                  height="250"
-                  width="150"
-                  image={MilkImage}
-                  alt="milk"
+    <section>
+      <Container className="milk__container">
+        <ToastContainer
+          autoClose={3000}
+          draggable={false}
+          icon={<CheckCircleIcon />}
+          className="green__toast"
+        />
+        <Grid container className="milk__grid">
+          {selectedMilk === null ? (
+            <form className="milk__search">
+              <label>
+                <TextField
+                  sx={{ m: 1, width: 300 }}
+                  className="milk__search-input"
+                  type="text"
+                  placeholder="Search milk..."
+                  onChange={handleSearch}
                 />
-                <CardContent>
-                  <Typography gutterBottom variant="subtitle1" component="div">
-                    {milk.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    className="milk__card-type-storage-container"
-                  >
-                    <span className="milk__card-type">{milk.type}</span>
-                    <span
-                      className={`milk__card-storage ${
-                        milk.storage < 10 ? "red" : ""
-                      }`}
-                    >
-                      {milk.storage} liters
-                    </span>
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))
-        )}
-        {selectedMilk && (
-          <Order
-            milk={selectedMilk}
-            setSelectedMilk={setSelectedMilk}
-            updateMilkData={updateMilkData}
-          />
-        )}
-      </div>
+              </label>
+              <FormControl>
+                <InputLabel>Milk Type</InputLabel>
+                <Select
+                  sx={{ m: 1, width: 300 }}
+                  value={selectedType}
+                  label="Filter by type"
+                  onChange={handleTypeSelection}
+                >
+                  <MenuItem value="">All</MenuItem>
+                  {Array.from(new Set(milkData.map((milk) => milk.type))).map(
+                    (type) => (
+                      <MenuItem value={type}>{type}</MenuItem>
+                    )
+                  )}
+                </Select>
+              </FormControl>
+              <div className="milk__counter">
+                <p>{numOfMilks} milks</p>
+              </div>
+            </form>
+          ) : null}
+          {selectedMilk ? (
+            <article className="milk__card">
+              <div className="milk__card-img-container">
+                <img src={MilkImage} alt="milk" className="milk__card-img" />
+              </div>
+            </article>
+          ) : (
+            filteredMilkData.map((milk) => (
+              <Grid item xs={12} sm={6} md={6} lg={4}>
+                <Card sx={{ m: 2, p:1}}>
+                  <CardActionArea onClick={() => setSelectedMilk(milk)}>
+                    <CardMedia
+                      component="img"
+                      height="250"
+                      width="150"
+                      image={MilkImage}
+                      alt="milk"
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="subtitle1"
+                        component="div"
+                      >
+                        {milk.name}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        className="milk__card-type-storage-container"
+                      >
+                        <span className="milk__card-type">{milk.type}</span>
+                        <span
+                          className={`milk__card-storage ${
+                            milk.storage < 10 ? "red" : ""
+                          }`}
+                        >
+                          {milk.storage} liters
+                        </span>
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))
+          )}
+          {selectedMilk && (
+            <Order
+              milk={selectedMilk}
+              setSelectedMilk={setSelectedMilk}
+              updateMilkData={updateMilkData}
+            />
+          )}
+        </Grid>
+      </Container>
     </section>
   );
 };
